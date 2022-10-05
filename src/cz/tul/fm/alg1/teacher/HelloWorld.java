220c819c10fb5abfdde1fc64fa05dcb2a4d160a4
Jana Vitvarova <jana.vitvarova@tul.cz>
2022-10-05 10:50:38 +0200

    Add HelloWorld with good package name

---
+++ src/cz/tul/fm/alg1/teacher/HelloWorld.java
@@ -0,0 +1,17 @@
+package cz.tul.fm.alg1.teacher;
+
+/**
+ * My first program to test that everything works
+ * @author teacher
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+        //System.out.println("Hello World!" + args[0]);
+    }
+    
+}
