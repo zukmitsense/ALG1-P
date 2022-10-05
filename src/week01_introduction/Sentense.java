f2ef0f80394d192138f8386f9df01d80d6f77197
Jana Vitvarova <jana.vitvarova@tul.cz>
2022-10-05 10:51:38 +0200

    Add week01_introduction

---
+++ src/week01_introduction/Sentense.java
@@ -0,0 +1,15 @@
+package week01_introduction;
+
+public class Sentense {
+    public static void main(String[] args) {
+        String name;
+        int yearBirth;
+        char code;
+        
+        name = "Filip";
+        yearBirth = 2009;
+        code = 'A';
+        
+        System.out.println("Dite" + name + " ma " + (2022-yearBirth) +"let");
+    }
+}
